06a18e8b75cd8cd09f27a3f69e17199667a4a1bb
Carmen <a18107090@idat.edu.pe>
2019-10-30 11:32:17 -0500

    + Mensaje en consola

--- src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
+++ src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
@@ -6,7 +6,7 @@ public class App {
     
     public static void main (String [] args) {
         
-        System.out.println("fd");
+        System.out.println("Hola Carmen");
         
         
         
